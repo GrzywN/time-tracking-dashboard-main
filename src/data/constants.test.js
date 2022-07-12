6b213eec97093d7bf5f2774d5f1228e8ebbd7e47
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-12 23:08:43 +0200

    add 2 constants.js tests

--- src/data/constants.test.js
+++ src/data/constants.test.js
@@ -1,5 +1,12 @@
 import { describe, it, expect } from "vitest";
-import { DEFAULT_TIMEFRAME, DAILY, WEEKLY, MONTHLY } from "./constants";
+import {
+  DEFAULT_TIMEFRAME,
+  DAILY,
+  WEEKLY,
+  MONTHLY,
+  PHOTO_URL,
+  ELLIPSIS_URL,
+} from "./constants";
 
 describe("constants", () => {
   it("should be a predefined timeframe (DEFAULT_TIMEFRAME equal WEEKLY)", () => {
@@ -16,5 +23,13 @@ describe("constants", () => {
 
   it("should be a predefined timeframe (MONTHLY)", () => {
     expect(MONTHLY).toEqual(MONTHLY);
+  });
+
+  it("should be a predefined URL (PHOTO_URL)", () => {
+    expect(PHOTO_URL).toEqual("./src/assets/image-jeremy.png");
+  });
+
+  it("should be a predefined URL (ELLIPSIS_URL)", () => {
+    expect(ELLIPSIS_URL).toEqual("./src/assets/icon-ellipsis.svg");
   });
 });
