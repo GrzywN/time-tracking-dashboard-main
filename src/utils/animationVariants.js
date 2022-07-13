af66e0c56706208db0360d659f306a6619f640bf
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-13 11:17:56 +0200

    toggle off animations, change tailwind config so svgs can load

--- src/utils/animationVariants.js
+++ src/utils/animationVariants.js
@@ -1,34 +1,34 @@
 export const createOpacityVariants = (number = 1) => {
   const variants = [];
-  for (let i = 0; i < number; i += 1) {
-    variants.push({
-      initial: { opacity: 0 },
-      animate: {
-        opacity: 1,
-        transition: {
-          duration: 1,
-          delay: i * 0.25 + 0.5,
-        },
-      },
-    });
-  }
+  // for (let i = 0; i < number; i += 1) {
+  //   variants.push({
+  //     initial: { opacity: 0 },
+  //     animate: {
+  //       opacity: 1,
+  //       transition: {
+  //         duration: 1,
+  //         delay: i * 0.25 + 0.5,
+  //       },
+  //     },
+  //   });
+  // }
   return variants;
 };
 
 export const scaleFromBottom = {
-  initial: { scaleY: 0, transformOrigin: "bottom" },
-  animate: {
-    scaleY: 1,
-    transition: { duration: 1, type: "spring" },
-  },
+  // initial: { scaleY: 0, transformOrigin: "bottom" },
+  // animate: {
+  //   scaleY: 1,
+  //   transition: { duration: 1, type: "spring" },
+  // },
 };
 
 export const scaleFromTop = {
-  initial: { scaleY: 0, transformOrigin: "top" },
-  animate: {
-    scaleY: 1,
-    transition: { duration: 1, type: "spring" },
-  },
+  // initial: { scaleY: 0, transformOrigin: "top" },
+  // animate: {
+  //   scaleY: 1,
+  //   transition: { duration: 1, type: "spring" },
+  // },
 };
 
 export const cardHover = {
